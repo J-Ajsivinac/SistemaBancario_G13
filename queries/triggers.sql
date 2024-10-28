57b4e6378a75d542b1c70d0fe6195c959a094eac
J-Ajsivinac <meso.israelajsivinac@gmail.com>
2024-10-27 23:57:21 -0600

    Add triggers for updating balances and handling deposits and transfers

--- queries/triggers.sql
+++ queries/triggers.sql
@@ -45,3 +45,57 @@ BEGIN
     END IF;
 END;
 /
+
+CREATE OR REPLACE PROCEDURE manejar_deposito (
+    p_numero_de_cuenta IN VARCHAR2,
+    p_monto IN NUMBER
+) AS
+BEGIN
+    -- Actualiza el saldo de la cuenta
+    UPDATE CUENTAS
+    SET SALDO = SALDO + p_monto
+    WHERE NUMERO_DE_CUENTA = p_numero_de_cuenta;
+
+    -- Registra la transacción
+    INSERT INTO TRANSACCIONES (NUMERO_DE_CUENTA_ORIGEN, MONTO, FECHA, HORA, DESCRIPCION)
+    VALUES (p_numero_de_cuenta, p_monto, SYSDATE, SYSTIMESTAMP, 'Depósito');
+    
+    COMMIT; -- Asegura que los cambios se persistan
+END manejar_deposito;
+/
+
+
+CREATE OR REPLACE PROCEDURE manejar_transferencia (
+    p_id_sucursal_origen IN NUMBER,
+    p_id_sucursal_destino IN NUMBER,
+    p_monto IN NUMBER
+) AS
+BEGIN
+    -- Verifica que haya suficiente saldo en la sucursal origen
+    DECLARE
+        v_fondos_disponibles NUMBER;
+    BEGIN
+        SELECT FONDOS_DISPONIBLES INTO v_fondos_disponibles FROM BOVEDAS WHERE ID_AGENCIA_SUCURSAL = p_id_sucursal_origen;
+
+        IF v_fondos_disponibles >= p_monto THEN
+            -- Actualiza el saldo de la sucursal origen
+            UPDATE BOVEDAS
+            SET FONDOS_DISPONIBLES = FONDOS_DISPONIBLES - p_monto
+            WHERE ID_AGENCIA_SUCURSAL = p_id_sucursal_origen;
+
+            -- Actualiza el saldo de la sucursal destino
+            UPDATE BOVEDAS
+            SET FONDOS_DISPONIBLES = FONDOS_DISPONIBLES + p_monto
+            WHERE ID_AGENCIA_SUCURSAL = p_id_sucursal_destino;
+
+            -- Registra la transacción interbancaria
+            INSERT INTO TRANSACCIONES_INTERBANCARIAS (ID_SUCURSAL_AGENCIA_ORIGEN, ID_SUCURSAL_AGENCIA_DESTINO, MONTO, FECHA_HORA, DESCRIPCION)
+            VALUES (p_id_sucursal_origen, p_id_sucursal_destino, p_monto, SYSTIMESTAMP, 'Transferencia entre sucursales');
+        ELSE
+            RAISE_APPLICATION_ERROR(-20002, 'Fondos insuficientes en la sucursal origen para la transferencia.');
+        END IF;
+    END;
+
+    COMMIT; -- Asegura que los cambios se persistan
+END manejar_transferencia;
+/
